39d510d0d0888979dc145ca53ac0e66706458cde
Julie Kim <118621382+jjuliekim@users.noreply.github.com>
2024-05-31 01:03:45 -0400

    tour information

--- app/src/main/java/com/example/kim_j_project4/DashboardActivity.java
+++ app/src/main/java/com/example/kim_j_project4/DashboardActivity.java
@@ -2,6 +2,7 @@ package com.example.kim_j_project4;
 
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.lifecycle.Observer;
@@ -13,8 +14,10 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -39,6 +42,7 @@ public class DashboardActivity extends AppCompatActivity implements OnMapReadyCa
     private String username;
     private DashboardViewModel dashboardViewModel;
     private LocationManager locationManager;
+    private String mediaPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,15 +57,28 @@ public class DashboardActivity extends AppCompatActivity implements OnMapReadyCa
         // set greeting text
         Intent myIntent = getIntent();
         username = myIntent.getStringExtra("username");
-        TextView greetingText = findViewById(R.id.greeting);
-        greetingText.setText(String.format("Welcome %s", username));
+        TextView welcomeText = findViewById(R.id.welcome_text);
+        welcomeText.setText(String.format("Welcome, %s!", username));
 
         dashboardViewModel = new ViewModelProvider(this).get(DashboardViewModel.class);
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
+        // add media button
+        Button addMediaButton = findViewById(R.id.add_media_button);
+        addMediaButton.setOnClickListener(v -> pickMedia());
+
         // save tour button
         Button saveButton = findViewById(R.id.save_tour_button);
         saveButton.setOnClickListener(v -> saveTour());
+
+        // view tours button
+        Button viewToursButton = findViewById(R.id.view_tours_button);
+        viewToursButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // view tours
+            }
+        });
 
         // refreshing/updating map
         dashboardViewModel.getLocations().observe(this, locations -> {
@@ -78,11 +95,6 @@ public class DashboardActivity extends AppCompatActivity implements OnMapReadyCa
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        /*// Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
-
         // check permissions
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -92,45 +104,39 @@ public class DashboardActivity extends AppCompatActivity implements OnMapReadyCa
 
         // get user's current location and move camera there
         mMap.setMyLocationEnabled(true);
-        goToCurrLocation();
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                Log.i("HERE DASHBOARD", "curr loc: " + location.getLatitude() + ", " + location.getLongitude());
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15));
+                locationManager.removeUpdates(this);
+            }
+        });
 
         // allow users to mark locations of interest on the map (store that in ViewModel)
         mMap.setOnMapClickListener(location -> dashboardViewModel.addLocation(location));
 
     }
 
-    // get user's current location
-    private void goToCurrLocation() {
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
-                @Override
-                public void onLocationChanged(@NonNull Location location) {
-                    LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
-                    Log.i("HERE DASHBOARD", "curr loc: " + location.getLatitude() + ", " + location.getLongitude());
-                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15));
-                    locationManager.removeUpdates(this);
-                }
-            });
-        }
-    }
-
     // save tour information
     private void saveTour() {
         EditText nameEditText = findViewById(R.id.tour_name);
         EditText descriptionEditText = findViewById(R.id.tour_description);
-        ArrayList<LatLng> locations = dashboardViewModel.getLocations().getValue();
+        EditText webLinkEditText = findViewById(R.id.tour_link);
         String tourName = nameEditText.getText().toString();
         String tourDescription = descriptionEditText.getText().toString();
+        String webLink = webLinkEditText.getText().toString();
+        ArrayList<LatLng> locations = dashboardViewModel.getLocations().getValue();
 
         // check for valid entries
         if (tourName.isEmpty() || tourDescription.isEmpty() || locations == null || locations.isEmpty()) {
-            Toast.makeText(this, "Please fill in all details and mark locations on the map", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Empty Entries", Toast.LENGTH_SHORT).show();
             return;
         }
 
         // save tour to json file
-        Tour tour = new Tour(tourName, tourDescription, locations);
+        Tour tour = new Tour(tourName, tourDescription, webLink, mediaPath, locations);
         Gson gson = new Gson();
         String tourJson = gson.toJson(tour);
         try (FileWriter writer = new FileWriter(getFilesDir() + "/" + tourName + ".json")) {
@@ -138,6 +144,24 @@ public class DashboardActivity extends AppCompatActivity implements OnMapReadyCa
             Toast.makeText(this, "Tour saved successfully", Toast.LENGTH_SHORT).show();
         } catch (IOException e) {
             Toast.makeText(this, "Failed to save tour", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // pick media
+    private void pickMedia() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        startActivityForResult(intent, 2);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 2 && resultCode == RESULT_OK && data != null) {
+            Uri selectedMediaUri = data.getData();
+            if (selectedMediaUri != null) {
+                mediaPath = selectedMediaUri.toString();
+            }
         }
     }
 }
--- app/src/main/java/com/example/kim_j_project4/Tour.java
+++ app/src/main/java/com/example/kim_j_project4/Tour.java
@@ -1,17 +1,23 @@
 package com.example.kim_j_project4;
 
 import com.google.android.gms.maps.model.LatLng;
+
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Tour {
+public class Tour implements Serializable {
     private String name;
     private String description;
+    private String webLink;
+    private String mediaPath;
     private ArrayList<LatLng> locations;
 
     // constructor
-    public Tour(String name, String description, ArrayList<LatLng> locations) {
+    public Tour(String name, String description, String webLink, String mediaPath, ArrayList<LatLng> locations) {
         this.name = name;
         this.description = description;
+        this.webLink = webLink;
+        this.mediaPath = mediaPath;
         this.locations = locations;
     }
 
@@ -28,12 +34,28 @@ public class Tour {
         return locations;
     }
 
+    public String getWebLink() {
+        return webLink;
+    }
+
+    public String getMediaPath() {
+        return mediaPath;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public void setWebLink(String webLink) {
+        this.webLink = webLink;
+    }
+
+    public void setMediaPath(String mediaPath) {
+        this.mediaPath = mediaPath;
     }
 
     public void setLocations(ArrayList<LatLng> locations) {
